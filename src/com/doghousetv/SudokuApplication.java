4bd8f9f9fe89aef1f4c6b47e708fc6ba44e6626d
DOGHOUSETV <arubigeorge0@gmail.com>
2020-12-22 16:43:35 -0500

    Update SudokuApplication.java

--- src/com/doghousetv/SudokuApplication.java
+++ src/com/doghousetv/SudokuApplication.java
@@ -1,9 +1,7 @@
 package com.doghousetv;
-
+import com.doghousetv.userinterface.IUserInterfaceContract;
+import com.doghousetv.userinterface.UserInterfaceImpl;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
