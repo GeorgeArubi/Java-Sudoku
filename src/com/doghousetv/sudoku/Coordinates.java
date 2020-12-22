2e99cdbb14218541ef5f1920cb4e34667d6dfad5
DOGHOUSETV <arubigeorge0@gmail.com>
2020-12-22 13:35:08 -0500

    Create Coordinates.java

---
+++ src/com/doghousetv/sudoku/Coordinates.java
@@ -0,0 +1,33 @@
+package com.doghousetv.sudoku;
+
+public class Coordinates {
+    private final int x;
+    private final int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x &&
+                y == that.y;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(x, y);
+    }
+}
