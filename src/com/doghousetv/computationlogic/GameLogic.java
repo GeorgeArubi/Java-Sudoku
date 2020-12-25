3d916282680022523606407c2e83342974beeaf8
DOGHOUSETV <arubigeorge0@gmail.com>
2020-12-25 08:42:02 -0500

    Update GameLogic.java

--- src/com/doghousetv/computationlogic/GameLogic.java
+++ src/com/doghousetv/computationlogic/GameLogic.java
@@ -1,4 +1,32 @@
 package com.doghousetv.computationlogic;
 
+import com.doghousetv.constants.GameState;
+import com.doghousetv.sudoku.SudokuGame;
+
 public class GameLogic {
+
+    public static SudokuGame getNewGame() {
+        return new SudokuGame(
+                GameState.NEW,
+                GameGenerator.getNewGameGrid()
+        );
+    }
+
+    public static GameState checkForCompletion(int[][] grid) {
+        if (sudokuIsInvalid(grid))
+            return GameState.ACTIVE;
+
+        if (tilesAreNotFilled(grid))
+            return GameState.ACTIVE;
+
+        return GameState.COMPLETE;
+    }
+
+    private static boolean sudokuIsInvalid(int[][] grid) {
+        return false;
+    }
+
+    private static boolean tilesAreNotFilled(int[][] grid) {
+        return false;
+    }
 }
