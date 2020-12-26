11f658303d284cc783707893cc84e44294314d7d
DOGHOUSETV <arubigeorge0@gmail.com>
2020-12-26 00:12:59 -0500

    Update SudokuBuildLogic.java

--- src/com/doghousetv/buildlogic/SudokuBuildLogic.java
+++ src/com/doghousetv/buildlogic/SudokuBuildLogic.java
@@ -1,4 +1,28 @@
 package com.doghousetv.buildlogic;
 
+import com.doghousetv.computationlogic.GameLogic;
+import com.doghousetv.sudoku.IStorage;
+import com.doghousetv.sudoku.SudokuGame;
+import com.doghousetv.userinterface.IUserInterfaceContract;
+import com.doghousetv.userinterface.logic.ControlLogic;
+
+import java.io.IOException;
+
 public class SudokuBuildLogic {
+    public static void build(IUserInterfaceContract.View userInterface) throws IOException {
+        SudokuGame initialState;
+        IStorage storage = new LocalStorageImpl();
+
+        try {
+            initialState = storage.getGameData();
+        } catch (IOException e) {
+            initialState = GameLogic.getNewGame();
+            storage.updateGameData(initialState);
+        }
+
+        IUserInterfaceContract.EventListener uiLogic = new ControlLogic(storage, userInterface);
+
+        userInterface.setListener(uiLogic);
+        userInterface.updateBoard(initialState);
+    }
 }
